7804fadc9c9b5ce055fca654e3c20c2a35cacb6f
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-18 22:59:09 +0300

    add migrations

--- ToDoList.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ToDoList.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ ToDoList.Data/Migrations/20200818195809_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoListItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Content = table.Column<string>(maxLength: 1000, nullable: false),
+                    DuetoDateTime = table.Column<DateTime>(type: "date", nullable: false),
+                    Done = table.Column<bool>(nullable: false),
+                    TodoListItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoItems_TodoListItems_TodoListItemId",
+                        column: x => x.TodoListItemId,
+                        principalTable: "TodoListItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_TodoListItemId",
+                table: "TodoItems",
+                column: "TodoListItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "TodoListItems");
+        }
+    }
+}
