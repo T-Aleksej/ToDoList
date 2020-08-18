0f5463ab4e8e24ea84074e2d3863b435a013dd21
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-18 23:05:10 +0300

    create extension method AddCustomDbContext

---
+++ ToDoList.API/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Core.Context;
+
+namespace ToDoList.API.Extensions
+{
+    public static class CustomExtensionMethods
+    {
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TodoListContext>(options =>
+            {
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TodoList;MultipleActiveResultSets=True");
+            });
+
+            return services;
+        }
+    }
+}
