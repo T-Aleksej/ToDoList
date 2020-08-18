366d1c866b937221ac19995c867a3029c1b992a5
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-18 22:57:28 +0300

    add db design context

---
+++ ToDoList.Core/Context/TodoListContextDesignFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ToDoList.Core.Context
+{
+    public class TodoListContextDesignFactory : IDesignTimeDbContextFactory<TodoListContext>
+    {
+        public TodoListContext CreateDbContext(string[] args)
+        {
+            //var optionsBuilder = new DbContextOptionsBuilder<TodoListContext>()
+            //    .UseInMemoryDatabase("TodoList");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TodoListContext>()
+                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TodoList;MultipleActiveResultSets=True",
+                x => x.MigrationsAssembly("ToDoList.Data"));
+
+            return new TodoListContext(optionsBuilder.Options);
+        }
+    }
+}
