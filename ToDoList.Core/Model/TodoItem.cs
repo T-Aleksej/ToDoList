966122aded34aad528703559899382a53902b2dc
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-18 22:49:06 +0300

    add model

---
+++ ToDoList.Core/Model/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ToDoList.Core.Model
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DuetoDateTime { get; set; }
+        public bool Done { get; set; }
+
+        public int TodoListItemId { get; set; }
+        public TodoListItem TodoListItem { get; set; }
+    }
+}
---
+++ ToDoList.Core/Model/TodoListItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.Core.Model
+{
+    public class TodoListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<TodoItem> TodoItems { get; set; }
+    }
+}
