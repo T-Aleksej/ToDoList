3fa2660c194ae7530859db88ecf61a7e40ae3232
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-20 19:42:56 +0300

    rename property

--- ToDoList.API/Infrastructure/TodoItemQueryParameters.cs
+++ ToDoList.API/Infrastructure/TodoItemQueryParameters.cs
@@ -4,9 +4,9 @@ namespace ToDoList.API.Infrastructure
 {
     public class TodoItemQueryParameters : QueryParameters
     {
-        public int TodoListItemId { get; set; }
+        public int TodoListItem { get; set; }
         public string Title { get; set; }
-        public DateTime? DateOfCompletion { get; set; }
+        public DateTime? Date { get; set; }
         public bool? IsComplete { get; set; }
     }
 }
