fdaa103b4e2d6c69f280168757d056979d52984f
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-20 03:14:34 +0300

    add extension method for iqueryable

---
+++ ToDoList.API/Extensions/QueryableExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ToDoList.API.Extensions
+{
+    public static class QueryableExtensionMethods
+    {
+        public static IQueryable<T> WhereIf<T>(
+            this IQueryable<T> queryable,
+            Expression<Func<T, bool>> selector,
+            Func<bool> condition)
+        {
+            return condition() ? queryable.Where(selector) : queryable;
+        }
+    }
+}
