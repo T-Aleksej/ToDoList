41cc5241f0dd38275738799f1b3b8469db3f08b0
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-20 03:13:11 +0300

    add todoItem query parameters

---
+++ ToDoList.API/Infrastructure/TodoItemQueryParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoList.API.Infrastructure
+{
+    public class TodoItemQueryParameters : QueryParameters
+    {
+        public int TodoListItemId { get; set; }
+        public string Title { get; set; }
+        public DateTime? DateOfCompletion { get; set; }
+        public bool? IsComplete { get; set; }
+    }
+}
