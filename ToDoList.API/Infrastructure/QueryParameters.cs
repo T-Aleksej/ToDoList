ce5ab608a74a09104e6ac8e0bcd6f41f15db3576
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-20 03:11:41 +0300

    add base query parameters

---
+++ ToDoList.API/Infrastructure/QueryParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDoList.API.Infrastructure
+{
+    public class QueryParameters
+    {
+        const int _maxPageSize = 100;
+        private int _pageSize;
+
+        public int PageIndex { get; set; } = 1;
+        public int PageSize
+        {
+            get => Math.Max(_pageSize, 1);
+            set => _pageSize = Math.Min(_maxPageSize, value);
+        }
+    }
+}
