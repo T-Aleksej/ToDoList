7268368178fde371b5195d8a95c947f4e62d4d44
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-21 18:17:56 +0300

    create view model for pagination

---
+++ ToDoList.API/ViewModel/PaginatedItemsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.API.ViewModel
+{
+    public class PaginatedItemsViewModel<TEntity> where TEntity : class
+    {
+        public PaginatedItemsViewModel(int pageIndex, int pageSize, long totalitems, IEnumerable<TEntity> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Totalitems = totalitems;
+            Data = data;
+        }
+
+        public int PageIndex { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public long Totalitems { get; private set; }
+
+        public IEnumerable<TEntity> Data { get; private set; }
+    }
+}
