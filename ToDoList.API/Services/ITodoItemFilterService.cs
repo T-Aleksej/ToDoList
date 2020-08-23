f145155bca13809d5a1cd6192f4ca1a921d00546
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-24 00:24:46 +0300

    add TodoItem filter service

---
+++ ToDoList.API/Services/ITodoItemFilterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.API.Infrastructure;
+using ToDoList.Core.Model;
+
+namespace ToDoList.API.Services
+{
+    public interface ITodoItemFilterService
+    {
+        Task<(IQueryable<T>, long)> FilteredByNameAndDateAndComplete<T>(IQueryable<T> todoItems, TodoItemQueryParameters queryParam) where T : TodoItem;
+    }
+}
