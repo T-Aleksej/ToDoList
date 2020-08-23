ffbe46900bcd56161960ebefff36b05e6b2b8c56
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-23 23:27:13 +0300

    add description public methods

--- ToDoList.API/Infrastructure/TodoItemQueryParameters.cs
+++ ToDoList.API/Infrastructure/TodoItemQueryParameters.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.API.Infrastructure
 {
     public class TodoItemQueryParameters : QueryParameters
     {
-        public int TodoListItem { get; set; }
+        /// <summary>
+        /// TodoListItem ID
+        /// </summary>
+        [Required] public int TodoListItem { get; set; }
+
+        /// <summary>
+        /// TodoItem title
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// TodoItem assigned due date
+        /// </summary>
         public DateTime? Date { get; set; }
+
+        /// <summary>
+        /// Is the task done
+        /// </summary>
         public bool? IsComplete { get; set; }
     }
 }
