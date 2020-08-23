e3e6ae44dcdcc64fba5f090e2a270ecd0ecf83bf
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-24 00:27:02 +0300

    add pagination extension

--- ToDoList.API/Extensions/QueryableExtensionMethods.cs
+++ ToDoList.API/Extensions/QueryableExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using ToDoList.API.Infrastructure;
 
 namespace ToDoList.API.Extensions
 {
@@ -12,6 +13,15 @@ namespace ToDoList.API.Extensions
             Func<bool> condition)
         {
             return condition() ? queryable.Where(selector) : queryable;
+        }
+
+        public static IQueryable<T> Pagination<T>(this IQueryable<T> todoItems, QueryParameters queryParam)
+        {
+            todoItems = todoItems
+               .Skip(queryParam.PageSize * (queryParam.PageIndex - 1))
+               .Take(queryParam.PageSize);
+
+            return todoItems;
         }
     }
 }
