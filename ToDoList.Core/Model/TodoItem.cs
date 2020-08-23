0f58bece07f8050e44b46ba62012b4473c742c00
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-23 23:26:47 +0300

    add description public methods

--- ToDoList.Core/Model/TodoItem.cs
+++ ToDoList.Core/Model/TodoItem.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Core.Model
 {
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime DuetoDateTime { get; set; }
-        public bool Done { get; set; }
+
+        /// <summary>
+        /// The title of the TodoItem
+        /// </summary>
+        [Required] [MaxLength(50)] public string Title { get; set; }
+
+        /// <summary>
+        /// The content of the TodoItem
+        /// </summary>
+        [Required] [MaxLength(1000)] public string Content { get; set; }
+
+        /// <summary>
+        /// Assigned due date
+        /// </summary>
+        [Required] public DateTime DuetoDateTime { get; set; }
+
+        /// <summary>
+        /// Is the task done
+        /// </summary>
+        [Required] public bool Done { get; set; }
 
         public int TodoListItemId { get; set; }
         public TodoListItem TodoListItem { get; set; }
