0068184972e7ffc2ede533aefa67472025fb7d56
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-23 23:26:32 +0300

    add description public methods

--- ToDoList.Core/Model/TodoListItem.cs
+++ ToDoList.Core/Model/TodoListItem.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Core.Model
 {
     public class TodoListItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        /// <summary>
+        /// The title of the TodoListItem
+        /// </summary>
+        [Required] [MaxLength(50)] public string Title { get; set; }
+
+        /// <summary>
+        /// The description of the TodoListItem
+        /// </summary>
+        [MaxLength(100)] public string Description { get; set; }
 
         public List<TodoItem> TodoItems { get; set; }
     }
