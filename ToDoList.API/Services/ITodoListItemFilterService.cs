60067683d7f1b63a0a0bf96dad0be1e6f1017ae6
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-24 00:24:18 +0300

    add TodoItem filter service

---
+++ ToDoList.API/Services/ITodoListItemFilterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.API.Infrastructure;
+using ToDoList.Core.Model;
+
+namespace ToDoList.API.Services
+{
+    public interface ITodoListItemFilterService
+    {
+        Task<(IQueryable<T>, long)> FilteredByName<T>(IQueryable<T> todoListItems, TodoListItemQueryParameters queryParam) where T : TodoListItem;
+    }
+}
---
+++ ToDoList.API/Services/TodoListItemFilterService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.API.Extensions;
+using ToDoList.API.Infrastructure;
+using ToDoList.Core.Model;
+
+namespace ToDoList.API.Services
+{
+    public class TodoListItemFilterService : ITodoListItemFilterService
+    {
+        public async Task<(IQueryable<T>, long)> FilteredByName<T>(IQueryable<T> todoListItems, TodoListItemQueryParameters queryParam) where T : TodoListItem
+        {
+            todoListItems = todoListItems.WhereIf(
+                x => x.Title.Trim().Contains(queryParam.Title.Trim()),
+                () => !string.IsNullOrWhiteSpace(queryParam.Title));
+
+            var totalitems = await todoListItems.LongCountAsync();
+
+            var page = todoListItems.Pagination(queryParam);
+
+            return (page, totalitems);
+        }
+    }
+}
