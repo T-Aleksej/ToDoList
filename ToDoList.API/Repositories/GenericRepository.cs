26c20676f52d96ed4a9f0a4f88d5370124900f16
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-26 17:04:55 +0300

    add default implementation interface

---
+++ ToDoList.API/Repositories/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.API.Repositories
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected GenericRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        protected readonly TContext _context;
+
+        public void Add(TEntity model) => _context.Set<TEntity>().Add(model);
+
+        public IQueryable<TEntity> GetQueryable() => _context.Set<TEntity>().AsQueryable();
+
+        public async Task<TEntity> GetByIdAsync(int id) => await _context.Set<TEntity>().FindAsync(id);
+
+        public void Remove(TEntity model) => _context.Set<TEntity>().Remove(model);
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+
+        public void Update(TEntity model) => _context.Entry<TEntity>(model).State = EntityState.Modified;
+    }
+}
