417acc02f9bc1923f3bbcf37c65986a57ee2d244
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-26 20:53:54 +0300

    add swagger settings

---
+++ ToDoList.API/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ToDoList.API.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        readonly IApiVersionDescriptionProvider provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new OpenApiInfo()
+                    {
+                        Title = $"A simple example TodoList API {description.ApiVersion}",
+                        Version = description.ApiVersion.ToString()
+                    });
+            }
+        }
+    }
+}
