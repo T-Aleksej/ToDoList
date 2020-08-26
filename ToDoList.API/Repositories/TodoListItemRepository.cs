59d68d0d6a4e566f9f1ecfe56db95edb85c3784f
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-08-26 17:07:18 +0300

    add concrete repository

---
+++ ToDoList.API/Repositories/TodoListItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using ToDoList.Core.Context;
+using ToDoList.Core.Model;
+
+namespace ToDoList.API.Repositories
+{
+    public class TodoListItemRepository : GenericRepository<TodoListItem, TodoListContext>, ITodoListItemRepository
+    {
+        public TodoListItemRepository(TodoListContext context) : base(context) { }
+    }
+}
