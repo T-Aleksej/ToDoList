1719c1e5988b790ea6a0b4c05ebdcbd0ee76ea25
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-09-03 13:12:41 +0300

    delete

--- ToDoList.API/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace ToDoList.API.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index() => new RedirectResult("~/swagger");
-    }
-}
