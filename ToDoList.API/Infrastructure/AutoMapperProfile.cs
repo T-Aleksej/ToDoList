84fa4e3aa7570289323ca3031556feea2b8e53fd
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-09-10 14:29:15 +0300

    add config for automapper

---
+++ ToDoList.API/Infrastructure/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ToDoList.Core.Entities;
+using ToDoList.Core.Models;
+
+namespace ToDoList.API.Infrastructure
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<TodoListItem, ListItem>();
+            CreateMap<TodoItem, Item>().ForMember(x => x.ListItemId, x => x.MapFrom(m => m.TodoListItemId));
+
+            CreateMap<ListItem, TodoListItem>();
+            CreateMap<Item, TodoItem>().ForMember(x => x.TodoListItemId, x => x.MapFrom(m => m.ListItemId));
+        }
+    }
+}
