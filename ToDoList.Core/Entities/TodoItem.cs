65a1d259c25a041f8cb118e64884ddf17df6ee89
Aleksej Terpickij <alex-imsit@yandex.ru>
2020-09-10 14:27:28 +0300

    remove data annotations

--- ToDoList.Core/Entities/TodoItem.cs
+++ ToDoList.Core/Entities/TodoItem.cs
@@ -1,40 +1,28 @@
 ﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using ToDoList.Core.Entities.Base;
 
 namespace ToDoList.Core.Entities
 {
     public class TodoItem : Identity
     {
-        //public int Id { get; set; }
-
         /// <summary>
         /// The title of the TodoItem
         /// </summary>
-        [Required(ErrorMessage = "Title is required")]
-        [MaxLength(50, ErrorMessage = "Title can't be longer than 50 characters")] 
         public string Title { get; set; }
 
         /// <summary>
         /// The content of the TodoItem
         /// </summary>
-        [Required(ErrorMessage = "Content is required")]
-        [MaxLength(1000, ErrorMessage = "Content can't be longer than 1000 characters")] 
         public string Content { get; set; }
 
         /// <summary>
         /// Assigned due date
         /// </summary>
-        [Required(ErrorMessage = "DuetoDateTime is required")]
-        [DataType(DataType.Date)] 
         public DateTime DuetoDateTime { get; set; }
 
         /// <summary>
         /// Is the task done
         /// </summary>
-        [Required(ErrorMessage = "Done is required")]
-        [DefaultValue(false)] 
         public bool Done { get; set; }
 
         public int TodoListItemId { get; set; }
--- ToDoList.Core/Entities/TodoListItem.cs
+++ ToDoList.Core/Entities/TodoListItem.cs
@@ -9,14 +9,11 @@ namespace ToDoList.Core.Entities
         /// <summary>
         /// The title of the TodoListItem
         /// </summary>
-        [Required(ErrorMessage = "Title is required")]
-        [MaxLength(50, ErrorMessage = "Title can't be longer than 50 characters")]
         public string Title { get; set; }
 
         /// <summary>
         /// The description of the TodoListItem
         /// </summary>
-        [MaxLength(100, ErrorMessage = "Description can't be longer than 100 characters")]
         public string Description { get; set; }
 
         public List<TodoItem> TodoItems { get; set; }
